29c75cebe0df87ec0b8cd725766667bb2f6bcf70
Robert Spralja <robert.spralja@gmail.com>
2026-03-07 17:02:53 +0100

    updated benchmark to verify intersection correctness

--- src/bin/benchmark.rs
+++ src/bin/benchmark.rs
@@ -1,15 +1,23 @@
+use psi::threaded::run_threaded;
 use psi::types::Element;
-use psi::threaded;
-
+use psi::verify::verify_intersection;
 
 use std::time::Instant;
 
 fn time_protocol(X: Vec<Element>, Y: Vec<Element>) -> f64 {
+  let X_copy = X.clone();
+  let Y_Copy = Y.clone();
+
   let start = Instant::now();
 
-  let intersection = threaded::run_threaded(X, Y);
+  let I = run_threaded(X, Y);
 
   let elapsed = start.elapsed();
+
+  assert!(
+    verify_intersection(X_copy, Y_Copy, I),
+    "PSI protocol returned incorrect intersection"
+  );
 
   elapsed.as_secs_f64()
 }
